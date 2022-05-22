e1f02233d593f310a6f3105e55b43e40fedf0ba2
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-22 16:30:21 +0200

    Estilos dinámicos con CSS Modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,40 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
-
-// Usando StyledComponent de forma dinámica (para la parte invalid)
-// Sustituimos abajo el <div></div> por este <FormControl></FormControl>
-//
-// El prop invalid se usa aquí. Notar que hemos quitado los estilos propios de invalid, ya que ahora se
-// usan de forma dinámica con los props
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? 'red' : 'black')}
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.invalid ? 'red' : '#ccc')};
-    background: ${(props) => (props.invalid ? '#ffd7d7' : 'transparent')}
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+import styles from './CourseInput.module.css';
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -59,16 +26,18 @@ const CourseInput = (props) => {
     props.onAddGoal(enteredValue);
   };
 
-  // Forma 2 de hacer la parte dinámica (invalid)
-  // Se pueden informar props en los styled components y utilizarlos en los backsticks (en los estilos) para
-  // cambiarlos fácilmente de forma dinámica
-  // Aquí se indica el prop invalid
+  // Notar la forma con la que se accede a form-control. Como no se puede usar la forma
+  // styles.form-control, se usa la forma con paréntesis para acceder a la propiedad.
+  // Es una forma propia de JavaScript
+  //
+  // Para obtener estilos dinámicos usando CSS Modules, hacemos uso de los backsticks y de variables
+  // con el nombre de las propiedades y ahora se pueden añadir clases adicionales condicionadas a lo que queramos.
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );

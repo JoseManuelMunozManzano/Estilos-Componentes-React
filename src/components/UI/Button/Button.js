23cabc4c1cf22ae52df492fb5865533197f0cf9c
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-22 16:23:24 +0200

    Usando CSS Modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,49 +1,29 @@
-import styled from 'styled-components';
+// CSS Modules
+// https://create-react-app.dev/docs/adding-a-css-modules-stylesheet/
+// Es una característica que esta disponible solo en proyectos que están configurados para soportarlos porque
+// necesitan una transformación de código que necesita realizarse antes de que el código se ejecute en el navegador.
+// Lo bueno es que los proyectos de React creados con create-react-app ya están configurados para admitir Módulos CSS.
 
-// Aunque los ficheros CSS se importan en ciertos componentes, la realidad es que su scope no es ese. Se podrían
-// usar en cualquier componente y esto puede dar lugar a errores si en distintos componentes las clases se acaban
-// llamando igual.
-// Para evitar esto, hay 2 posibilidades.
-// La primera es usar un paquete llamado Styled Components (https://styled-components.com/)
-// Es un paquete que ayuda a construir componentes con ciertos estilos ligados a ellos y que solo afectan a esos
-// componentes
-// Para instalarlo:
-// npm install --save styled-components
+// Cambia la forma de importar el CSS. Ahora se importa a un nombre
+// Y el nombre del fichero .css también cambia. Antes de la extensión .css hay que añadir .module
+// Esto es para que el proceso de compilación subyacente transforme el código para que CSS Modules funcione.
+import styles from './Button.module.css';
 
-// Esta sintaxis se llama Tagged Template Literal.
-// Es propia de JavaScript, no del paquete ni de React.
-// Lo que hace es: button es un método "especial" en este styled object. En vez de los paréntesis se usan los backsticks.
-// Se ejecuta como un método detrás de escena, y lo que se pase entre los backsticks se pasa al botón de una manera
-// especial.
-// Este método, y esto es lo interesante, devuelve un nuevo componente botón con un estilo indicado entre los backsticks
-// y con clases generadas con nombres que no se van a repetir en ningún componente.
-// Al botón que se devuelve se le pueden seguir aplicando los props, etc. que se pasen desde otro componente.
-
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-
-  // Usando styled components con Media Queries
-  @media (min-width: 768px) {
-    width: auto;
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+// Ahora las clases son dinámicas usando el nombre styles, un objeto, y se pueden usar todas las clases
+// definidas en el fichero .css como properties.
+// Si vamos al inspector de propiedades de Chrome, veremos que crea un nombre de clase único.
+//
+// Para cada componente el nombre de clase será único con lo que se asegura que el scope de un fichero .css ahora si
+// sera el del componente que lo importe.
+//
+// La clase generada se compone de: Componente_propertie__claveHash
+// Por ejemplo: Button_button__2lgkF
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;

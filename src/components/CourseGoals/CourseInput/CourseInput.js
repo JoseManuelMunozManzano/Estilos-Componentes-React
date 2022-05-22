cc56fb96cd710e529ce5309a3504329403a97915
José Manuel Muñoz Manzano <neil_mercury@yahoo.es>
2022-05-22 15:50:57 +0200

    Styled Components dinámicos - Primera forma

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,44 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+// Usando StyledComponent de forma dinámica (para la parte invalid)
+// Sustituimos abajo el <div></div> por este <FormControl></FormControl>
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background: #ffd7d7;
+  }
+
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -26,17 +63,14 @@ const CourseInput = (props) => {
     props.onAddGoal(enteredValue);
   };
 
-  // Como alternativa se puede agregar una nueva clase dinámicamente solo si el valor ingresado no es válido
-  // (clave invalid) y en el fichero CourseInput.css se configuran las propiedades de esa clase invalid.
-  //
-  // Esto tiene de bueno que se vuelve a trabajar solo con clases y archivos CSS y se pueden agregar/quitar clases
-  // dinámicamente muy fácilmente
+  // Forma 1 de hacer la parte dinámica (invalid)
+  // En FormControl sólo tenemos que indicar invalid o '' porque el resto del CSS ya forma parte de FormControl
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl className={!isValid ? 'invalid' : ''}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
